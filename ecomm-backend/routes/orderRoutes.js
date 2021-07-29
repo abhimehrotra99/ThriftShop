0e0253167461773ccf6dd82ea875e1bd200ee421
abhimehrotra99 <abhimehrotra99@gmail.com>
2021-07-30 00:12:20 +0530

    Added Order Screen

--- ecomm-backend/routes/orderRoutes.js
+++ ecomm-backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
-import { addOrderItems, getOrderById } from '../controllers/orderController.js'
+import { addOrderItems, getOrderById, updateOrderToPaid } from '../controllers/orderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 router.route('/').post(protect, addOrderItems)
+router.route('/:id/pay').get(protect, updateOrderToPaid)
 router.route('/:id').get(protect, getOrderById)
 
 export default router
