6878167aff991f3ff925978c4b445815dd1c2bc8
abhimehrotra99 <abhimehrotra99@gmail.com>
2021-08-04 16:48:59 +0530

    Added Pagination and Product Carousel

--- ecomm-backend/controllers/productController.js
+++ ecomm-backend/controllers/productController.js
@@ -5,6 +5,9 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler( async(req, res) => {
+    const pageSize = 20
+    const page = Number(req.query.pageNumber) || 1
+
     const keyword = req.query.keyword ? {
         name: {
             $regex: req.query.keyword,
@@ -12,8 +15,9 @@ const getProducts = asyncHandler( async(req, res) => {
         }
     } :  {}
 
-    const products = await Product.find({ ...keyword })
-    res.json(products)
+    const count = await Product.countDocuments({ ...keyword })
+    const products = await Product.find({ ...keyword }).limit(pageSize).skip(pageSize * (page-1))
+    res.json({ products, page, pages: Math.ceil(count/pageSize) })
 })
 
 
@@ -136,4 +140,15 @@ const createProductReview = asyncHandler( async(req, res) => {
     }
 })
 
-export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview }
+
+
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler( async(req, res) => {
+    const products = await Product.find({}).sort({ rating: -1 }).limit(3)
+    res.json(products)
+})
+
+
+export { getProducts, getProductById, deleteProduct, createProduct, updateProduct, createProductReview, getTopProducts }
